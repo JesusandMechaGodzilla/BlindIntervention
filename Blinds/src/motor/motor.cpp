3af8176c4c9c9792ccd4e8839ef51190ba9b94ed
Ritchieluk <33823229+Ritchieluk@users.noreply.github.com>
2018-09-16 03:29:27 -0400

    cleeeaanneeed eer up

--- Blinds/src/motor/motor.cpp
+++ Blinds/src/motor/motor.cpp
@@ -5,42 +5,42 @@ static THD_WORKING_AREA(wa_motor_thread, 256);
 static THD_FUNCTION(motor_thread, arg) {
 (void) arg;
 systime_t time = chVTGetSystemTimeX();
-Servo myservo;
-myservo.attach(2);
+Servo myServo;
+myServo.attach(2);
 //ServoPosition 0 is the position when the blinds are as closed down, whereas ServoPosition 180 is closed above
 int servoPosition = 90;
 
 while(true){
-Serial.println(servoPosition);
-time += MS2ST(2000);
-//code here
-//avgVal is the indoor
-//adjSetting is the effective user setting
-if(timeDay){
-  int increment = (adjSetting - avgVal)/10;
-  if(increment>4)
-    increment = 4;
-  if(servoPosition<90 && servoPosition >= 0){
-    servoPosition += increment;
-    if(servoPosition <0)
-      servoPosition = 0;
-    }
-  else{
-    servoPosition -= increment;
-    if(servoPosiiton>180)
-      servoPosition = 180;
-    }
-}
-else
-  servoPosition = 0;
-
-myservo.write(servoPosition);
-
-
- chThdSleepUntil(time);
-}
-
+  Serial.println("Current Servo Position: ");
+  Serial.println(servoPosition);
+  time += MS2ST(2000);
+  //avgVal is the indoor
+  //adjSetting is the effective user setting
+  if(timeDay){
+    int increment = (adjSetting - avgVal)/10;
+    if(increment>4)
+      increment = 4;
+    if(servoPosition<90 && servoPosition >= 0){
+      servoPosition += increment;
+      if(servoPosition <0)
+        servoPosition = 0;
+      }
+    else{
+      servoPosition -= increment;
+      if(servoPosiiton>180)
+        servoPosition = 180;
+      }
   }
+  else
+    servoPosition = 0;
+
+  myServo.write(servoPosition);
+
+
+   chThdSleepUntil(time);
+}
+
+}
 
 void motorSetup(){
   chThdCreateStatic(wa_motor_thread, sizeof(wa_motor_thread),
