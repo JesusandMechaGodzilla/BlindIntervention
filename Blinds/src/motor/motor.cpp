23d8554c9bf7b6b00b836a0c49bb304cc73821aa
Ritchieluk <33823229+Ritchieluk@users.noreply.github.com>
2018-09-16 01:12:48 -0400

    Motorbitches
    yeah

--- Blinds/src/motor/motor.cpp
+++ Blinds/src/motor/motor.cpp
@@ -1,15 +1,28 @@
 #include "motor.h"
+#include "Servo.h"
 static THD_WORKING_AREA(wa_motor_thread, 256);
 static THD_FUNCTION(motor_thread, arg) {
 (void) arg;
 systime_t time = chVTGetSystemTimeX();
-
+Servo myservo;
+myservo.attach(2);
+//ServoPosition 0 is the position when the blinds are as closed down, whereas ServoPosition 180 is closed above
+int servoPosition = 90;
 
 while(true){
 time += MS2ST(1000);
 //code here
-
-
+//avgVal is the indoor
+//adjSetting is the effective user setting
+if(timeDay){
+int increment = (adjSetting - avgVal)/10;
+if(servoPosition<90)
+  servoPosition += increment;
+else
+  servoPosition -= increment;  
+}
+else
+  servoPosition = 0;
 
 
 
--- Blinds/src/motor/motor.h
+++ Blinds/src/motor/motor.h
@@ -1,6 +1,6 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 #include "common.h"
-
+#include "Servo.h"
 void motor_setup();
 #endif
